fcf90d4d8a25a2904fd09e2a8b03bf243db43a66
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-05-10 22:35:04 +0000

    livraison

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.sidebar').mCustomscrollbar()
+
+$('.chip-container').slick({
+    slidesToshow: 15,
+    slidesToscroll: 2,
+    arrows: true,
+    variableWidth: true,
+    prevArrow:'<div></div>'
+});
+
